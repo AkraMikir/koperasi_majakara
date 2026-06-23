981818d55869a2f0eb4c98b7b447eac3c2c8e67c
AkraMikir <162798908+AkraMikir@users.noreply.github.com>
2026-06-23 21:02:28 +0700

    Delete check_models.php

--- check_models.php
+++
@@ -1,9 +0,0 @@
-<?php
-$key = "AQ.Ab8RN6KuxDN01qxK3q9KemxrLlJZl30WgTRxBK5Ec5QdjvDVRQ";
-$url = "https://generativelanguage.googleapis.com/v1beta/models?key=" . $key;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$result = curl_exec($ch);
-curl_close($ch);
-echo $result;
